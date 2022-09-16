d08e805b00d14309601ead196de4a3feb9bf45bd
viruha <maurocastro1@gmail.com>
2022-09-16 16:28:48 -0300

    GET y SET

--- src/tp2/Combustible.java
+++ src/tp2/Combustible.java
@@ -8,4 +8,22 @@ public class Combustible {
 
     public Combustible(String tipo, Double precio) {
     }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+    
+    
 }
